dea301e127aca8b7f647a7255085e1634c6a0068
turalhuseyinzade <turalgithub@gmail.com>
2026-04-01 19:23:07 +0400

    Initial commit

---
+++ src/main/java/com/demo/TuralFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuralFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuralFinalProjectApplication.class, args);
+    }
+
+}
